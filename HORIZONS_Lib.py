c22a902a4505db79f8456aaa4199288511df45d2
SEVERUS\Vincent <delortvincent@live.fr>
2018-12-22 15:47:50 +0100

    work progression

--- HORIZONS_Lib.py
+++ HORIZONS_Lib.py
@@ -5,5 +5,11 @@ import Core_Lib as Core
 
 # === CLASSES ==========================================================================================================
 class HorizonsCharacter(Core.GenericCharacter):
-    def __init__(self, name):
+    def __init__(self, name, dexterity, constitution, stamina, reflexes, perception, erudition, concentration,
+                 ingeniosity, charisma, persuasion, empathy):
         Core.GenericCharacter.__init__(self, name)
+
+
+class CharAttribute:
+    def __init__(self, value):
+        self.value = value
